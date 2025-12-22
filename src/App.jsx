617b49d76df28f30d000b293cdb6e38d74b66d60
naman <nmnagrawal99@gmail.com>
2025-12-22 21:30:08 +0530

    routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from "react";
-
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
-
   return (
     <>
-      <h1>Vite + React</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
